bda94e37327ed26442042573052d05bf3ca16d4e
SunMk1 <kmk3296@gmail.com>
2022-11-22 18:19:24 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/ContactController.java
@@ -23,4 +23,38 @@ public class ContactController {
     contacts[size++] = name + "," + email + "," + tel + "," + company;
     return size;
   }
+
+  @GetMapping("/contact/get")
+  public Object get(String email) {
+    for (int i = 0; i < size; i++) {
+      if (email.equals(contacts[i].split(",")[1])) {
+        return contacts[i];
+      }
+    }
+    return 1;
+  }
+
+  @GetMapping("/contact/update")
+  public Object update(String name, String email, String tel, String company) {
+    String contact = name + "," + email + "," + tel + "," + company;
+    for (int i = 0; i < size; i++) {
+      if (email.equals(contacts[i].split(",")[1])) {
+        contacts[i] = contact;
+      }
+    }
+    return 1;
+  }
+
+  @GetMapping("/contact/delet")
+  public Object delet(String email) {
+    for (int i = 0; i < size; i++) {
+      if (email.equals(contacts[i].split(",")[1])) {
+        for (int j = i + 1; j < size; j++) {
+          contacts[j - 1] = contacts[j];
+        }
+        size--;
+      }
+    }
+    return 1;
+  }
 }
