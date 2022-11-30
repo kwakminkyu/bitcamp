c01466097d3473b8cd82d4abccc80b58094879f6
SunMk1 <kmk3296@gmail.com>
2022-11-30 21:34:12 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/ContactController.java
@@ -20,6 +20,19 @@ public class ContactController {
 
   @GetMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
+    // 배열이 꽉 찼는지 검사.
+    if (size == contacts.length) {
+      // 기존 배열 보다 50% 큰 배열을 새로 만든다.
+      int newCapacity = contacts.length + (contacts.length >> 1);
+      String[] arr = new String[newCapacity];
+
+      // 새 배열에 기존 배열의 값을 복사
+      for (int i = 0; i < contacts.length; i++ ) {
+        arr[i] = contacts[i];
+      }
+      // 연락처 배열을 기존 배열이 아닌 새 배열로 바꾼다.
+      contacts = arr;
+    }
     contacts[size++] = createCSV(name, email, tel, company);
     return size;
   }
