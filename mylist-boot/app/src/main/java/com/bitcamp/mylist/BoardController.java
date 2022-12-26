7dc4abe282c04c35c80a98b2d5f1b3ebb4a9f801
SunMk1 <kmk3296@gmail.com>
2022-12-26 13:13:24 +0900

    .

---
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/BoardController.java
@@ -0,0 +1,51 @@
+package com.bitcamp.mylist;
+
+import java.sql.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+  @GetMapping("/board/list")
+  public Object list() {
+    return ArrayList3.toArray();
+  }
+
+  @GetMapping("/board/add")
+  public Object add(Board board) {
+    board.setCreateDate(new Date(System.currentTimeMillis()));
+    ArrayList3.add(board);
+    return ArrayList3.size;
+  }
+
+  @GetMapping("/board/get")
+  public Object get(int index) {
+    if(index < 0 || index >= ArrayList3.size) {
+      return 0;
+    }
+    Board board = (Board) ArrayList3.list[index];
+    board.viewCount++;
+    return board;
+  }
+
+  @GetMapping("/board/update")
+  public Object update(int index, Board board) {
+    if(index < 0 || index >= ArrayList3.size) {
+      return 0;
+    }
+    Board old = (Board) ArrayList3.list[index];
+    board.viewCount = old.viewCount;
+    board.createDate = old.createDate;
+    return ArrayList3.set(index, board) == null ? 0 : 1;
+  }
+
+  @GetMapping("/board/delete")
+  public Object delet(int index) {
+    if(index < 0 || index >= ArrayList3.size) {
+      return 0;
+    }
+    ArrayList3.remove(index);
+    return 1;
+  }
+}
