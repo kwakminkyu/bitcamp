314db789a20a887bae8f185b37717440eee42203
SunMk1 <kmk3296@gmail.com>
2023-02-03 14:51:17 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.bitcamp.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.sql.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +21,25 @@ public class BoardController {
     boardList = new ArrayList();
     System.out.println("BoardController() 호출됨!");
 
-    FileReader in = new FileReader("boards.csv");
-    BufferedReader in2 = new BufferedReader(in);
+    FileInputStream in = new FileInputStream("boards.data");
+    BufferedInputStream in1 = new BufferedInputStream(in);
+    DataInputStream in2 = new DataInputStream(in1);
 
     String line;
-    while ((line = in2.readLine()) != null) {
-      boardList.add(Board.valueOf(line));
+    while (true) {
+      try {
+        Board board = new Board();
+        board.setTitle(in2.readUTF());
+        board.setContent(in2.readUTF());
+        board.setViewCount(in2.readInt());
+        board.setCreateDate(Date.valueOf(in2.readUTF()));
+
+        boardList.add(board);
+      } catch (Exception e) {
+        break;
+      }
+      in2.close();
     }
-    in2.close();
   }
 
   @RequestMapping("/board/list")
@@ -72,14 +85,17 @@ public class BoardController {
 
   @RequestMapping("/board/save")
   public Object save() throws Exception {
-    FileWriter out = new FileWriter("boards.csv");
-
-    PrintWriter out2 = new PrintWriter(out);
+    FileOutputStream out = new FileOutputStream("boards.data");
+    BufferedOutputStream out1 = new BufferedOutputStream(out);
+    DataOutputStream out2 = new DataOutputStream(out1);
 
     Object[] arr = boardList.toArray();
     for (Object obj : arr) {
       Board board = (Board) obj;
-      out2.println(board.toCsvString());
+      out2.writeUTF(board.getTitle());
+      out2.writeUTF(board.getContent());
+      out2.writeInt(board.getViewCount());
+      out2.writeUTF(board.getCreateDate().toString());
     }
     out2.close();
     return arr.length;
