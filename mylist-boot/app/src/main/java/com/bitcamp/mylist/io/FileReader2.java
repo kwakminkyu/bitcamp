b779d67d648984f593011c655a992b0996fbe805
SunMk1 <kmk3296@gmail.com>
2023-02-03 13:44:47 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp/mylist/io/FileReader2.java
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/io/FileReader2.java
@@ -1,16 +1,20 @@
 package com.bitcamp.mylist.io;
 
-public class FileReader2 extends java.io.FileReader {
+import java.io.FileReader;
+
+public class FileReader2 {
+
+  FileReader in;
 
   public FileReader2(String filename) throws Exception {
-    super(filename);
+    in = new FileReader(filename);
   }
 
   public String readLine() throws Exception {
     StringBuilder buf = new StringBuilder();
     int c;
 
-    while ((c = this.read()) != -1) {
+    while ((c = in.read()) != -1) {
       if (c == '\n') {
         return buf.toString();
       } else if (c == '\r') {
@@ -19,5 +23,9 @@ public class FileReader2 extends java.io.FileReader {
       }
     }
     return buf.toString();
+  }
+
+  public void close() throws Exception {
+    in.close();
   }
 }
