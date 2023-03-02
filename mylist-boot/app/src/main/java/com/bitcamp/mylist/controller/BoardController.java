9b8e5550b709799c6171016bcd9b37a7bd8aac18
SunMk1 <kmk3296@gmail.com>
2023-03-02 19:23:42 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/controller/BoardController.java
@@ -22,51 +22,43 @@ public class BoardController {
 
   @RequestMapping("/board/list")
   public Object list() {
-    return boardService.list();
+    return new ResultMap().setStatus(SUCCESS).setData(boardService.list());
   }
 
   @RequestMapping("/board/add")
   public Object add(Board board, HttpSession session) {
+    System.out.println("BoardController.add() 호출됨!");
     Member member = (Member) session.getAttribute("loginUser");
-    if (member == null) {
-      return new ResultMap().setStatus(FAIL).setData("로그인 하지 않았습니다");
-    }
     board.setWriter(member);
     boardService.add(board);
-    return new ResultMap().setStatus(SUCCESS); 
+    return new ResultMap().setStatus(SUCCESS);
   }
 
   @RequestMapping("/board/get")
   public Object get(int no) {
     Board board = boardService.get(no);
-    if(board == null) {
-      return "";
+    if (board == null) {
+      return new ResultMap().setStatus(FAIL).setData("해당 번호의 게시글이 없습니다.");
     }
-    return board;
+    return new ResultMap().setStatus(SUCCESS).setData(board);
   }
 
   @RequestMapping("/board/update")
   public Object update(Board board, HttpSession session) {
     Member member = (Member) session.getAttribute("loginUser");
-    if (member == null) {
-      return new ResultMap().setStatus(FAIL).setData("로그인 하지 않았습니다");
-    }
     board.setWriter(member);
     int count = boardService.update(board);
 
     if (count == 1) {
       return new ResultMap().setStatus(SUCCESS);
     } else {
-      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 작성자가 아닙니다");
+      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 작성자가 아닙니다.");
     }
   }
 
   @RequestMapping("/board/delete")
   public Object delete(int no, HttpSession session) {
     Member member = (Member) session.getAttribute("loginUser");
-    if (member == null) {
-      return new ResultMap().setStatus(FAIL).setData("로그인 하지 않았습니다");
-    }
     Board board = new Board();
     board.setNo(no);
     board.setWriter(member);
@@ -76,7 +68,7 @@ public class BoardController {
     if (count == 1) {
       return new ResultMap().setStatus(SUCCESS);
     } else {
-      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 작성자가 아닙니다");
+      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 작성자가 아닙니다.");
     }
   }
 }
---
+++ mylist-boot/app/src/main/java/com/bitcamp/mylist/interceptor/AuthInterceptor.java
@@ -0,0 +1,36 @@
+package com.bitcamp.mylist.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import com.bitcamp.mylist.controller.ResultMap;
+import com.bitcamp.mylist.domain.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AuthInterceptor implements HandlerInterceptor{
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    System.out.println("preHandle 호출");
+    HttpSession session =request.getSession();
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    if (loginUser == null) {
+      response.setContentType("application/json;charset=UTF-8");
+      response.getWriter().write(new ObjectMapper().writeValueAsString(new ResultMap()
+          .setStatus(ResultMap.FAIL)
+          .setData("로그인 하지 않았습니다")));
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+    System.out.println("postHandle 호출");
+    HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+  }
+}
